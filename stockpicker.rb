f4ff7905163c2f8ade4561e99dfcc0af283cd62b
Richard Kwon <rkminjae@gmail.com>
2020-11-03 18:31:35 +1300

    Change hardcoded array to stock_prices variable

--- stockpicker.rb
+++ stockpicker.rb
@@ -20,4 +20,5 @@ def stock_picker (days_array)
     p find_max_profit_days(max_profit_days)
 end
 
-stock_picker([17,3,6,9,15,8,6,1,10])
+stock_prices = [17,3,6,9,15,8,6,1,10]
+stock_picker(stock_prices)
